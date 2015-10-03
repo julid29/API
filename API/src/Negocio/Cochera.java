462b761674fa0179f1ce47dfce8317db9a007afe
Lucas Rivela <Lucas Rivela@desktop-lucas>
2015-10-03 18:59:12 -0300

    ..

--- API/src/Negocio/Cochera.java
+++ API/src/Negocio/Cochera.java
@@ -31,4 +31,7 @@ public class Cochera {
 		return numero;
 	}
 	
+	public boolean getEstado(){
+		return estado;
+	}
 }
--- API/src/Persistencia/AdministradorPersistenciaCochera.java
+++ API/src/Persistencia/AdministradorPersistenciaCochera.java
@@ -9,42 +9,33 @@ import Negocio.Cochera;
 public class AdministradorPersistenciaCochera {
 	private static AdministradorPersistenciaCochera pool;
 	
-	private AdministradorPersistenciaCochera()
-	{
+	private AdministradorPersistenciaCochera(){
 		
 	}
 	
-	public static AdministradorPersistenciaCochera getInstancia()
-	{
+	public static AdministradorPersistenciaCochera getInstancia(){
 		if (pool== null)
 			pool =new AdministradorPersistenciaCochera();
 		return pool;
 	}
 	
-	public void insert(Cochera c)
-	{
+	public void insert(Cochera c){
 		//Obtener conexion BD 
 		Connection con = PoolConnection.getPoolConnection().getConnection();
-		
 		//Generar sentencia SQL
-		try
-		{
+		try{
 			//Preparar sentencia insert en tabla destino
-			String senten = "INSERT INTO clientes (dni, nombre, domicilio, telefono) VALUES (?,?,?,?)" ;
+			String senten = "INSERT INTO clientes (numero, estado) VALUES (?,?)" ;
 			PreparedStatement ps = null;
 			ps = con.prepareStatement(senten);
-			
-			
+			ps.setString(1,c.getNumero());
+			ps.setBoolean(2,c.getEstado()); //EN LA BASE DE DATOS ESTADO DEBE SER UN BIT SEGUN LA DOCUMENTACION DE ORACLE
 			ps.execute();
-			
 			PoolConnection.getPoolConnection().closeConnections();
-			
-		}
-	      catch( SQLException e ) 
-	      {
+		}catch( SQLException e){
 				System.out.println("Mensaje Error al Insertar Cliente: " + e.getMessage());
 				System.out.println("Stack Trace al Insertar Cliente: " + e.getStackTrace());
 				PoolConnection.getPoolConnection().closeConnections();
-	      } 
+	    } 
 	}
 }
